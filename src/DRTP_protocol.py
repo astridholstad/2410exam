c52092d8a6ec9bb7dd096c50d862d7eadc5384c7
astridholstad <astridholstad21@gmail.com>
2025-04-29 18:58:13 +0200

    drtp

--- src/DRTP_protocol.py
+++ src/DRTP_protocol.py
@@ -1,1 +1,20 @@
 #common (core) functionality 
+import socket
+import time
+from .packet import Packet
+
+class DRTP:
+    """
+    This class will represent the base for the DRTP protocol implementation
+    """
+    def __init__(self, ip, port):
+        """
+        Constructor for the protocol, ip and port using a socket setup
+        """
+        self.ip = ip
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #note: using sock.dgram which is udp. will build my application on top of this, more on this later.
+        self.timeout = 0.4 #default 400 ms timeout
+        self.socket.settimeout(self.timeout)
+        
+    
