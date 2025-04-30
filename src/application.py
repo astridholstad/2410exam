e3a3c1653ab30ba2dceb8124ee0b7cb959f065fe
astridholstad <astridholstad21@gmail.com>
2025-04-30 09:10:44 +0200

    skeleton for main

--- src/application.py
+++ src/application.py
@@ -1,2 +1,39 @@
-#main method
+#main entry point in this appication 
 #command line parsing 
+
+import argparse
+import sys
+import os
+
+def parse_args():
+    """
+    This method will parse command line arguments 
+    Using argparse module
+    will return the arguments for the main method to use 
+    """
+    parser = argparse.ArgumentParser(description="DRTP File Transfer Application")
+
+
+def main():
+    """
+    Main method for this application 
+
+    """    
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
