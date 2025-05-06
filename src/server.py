8960ea1f33a820f97d14f1ef433e99a72332963b
astridholstad <astridholstad21@gmail.com>
2025-05-06 17:21:50 +0200

    comments on the server

--- src/server.py
+++ src/server.py
@@ -1,2 +1,19 @@
 #server functionality class
 import os
+from .DRTP_protocol import DRTP
+from .packet import Packet
+
+
+
+
+class Server:
+    """
+    This will be used to initialze the reciever with a  window size 
+    DRTP implementation of the server (the reviecer of the file)
+    The server need to handle ip, port, sliding window, file data, discarded sequence numbers. 
+
+
+    """
+
+    def __init__(self):
+        pass
