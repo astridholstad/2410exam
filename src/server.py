333e5c3c2cde5bb499325b20c27077753245b865
astridholstad <astridholstad21@gmail.com>
2025-05-13 11:27:32 +0200

    server

--- src/server.py
+++ src/server.py
@@ -2,6 +2,8 @@
 import os
 from .DRTP_protocol import DRTP
 from .packet import Packet
+import datetime
+import time
 
 
 
@@ -15,5 +17,25 @@ class Server:
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, ip, port, window_size=15, discarded_seq=None):
+        """
+        Constructor for the client with a set window size
+
+        """
+        super().__init__(ip, port)
+        self.recv_window= window_size
+        self.expct_seq_num = 1 #next expected sequence number
+        self.buffer = {} #buffer to store data, as a dict
+        self.discarded_seq = discarded_seq
+        self.discard_done = False # flag that is T/F to check if discard have been done.
+
+        #bind the socket door to the server address
+        self.socket.bind((ip, port))
+
+    def wait_for_handshake(self):
+        """
+        Wait for client to perform a three way handshake
+        
+        """
+
+    
