706781b0f7c23edd38004337c4fa8d863494d9f0
astridholstad <astridholstad21@gmail.com>
2025-05-13 12:14:19 +0200

    app.py

--- src/application.py
+++ src/application.py
@@ -16,18 +16,20 @@ def parse_args():
     parser = argparse.ArgumentParser(description="DRTP File Transfer Application")
 
     # need to write what mode we want the application to be in. 
-
-    #common command line args 
-    parser.add_argument('-i', '--ip', type=str, default='127.0.0.1', help='IP address')
-    parser.add_argument('-p', '--port', type=int, default=8080, help='Port number')
+    mode = parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument('-s', '--server', action='store_true', help='Run in server mode')
+    mode.add_argument('-c', '--client', action='store_true', help='Run in client mode')
 
     #command line args for client spesific functionality 
     parser.add_argument('-f', '--file', type=str, help='File you want to transfer')
     parser.add_argument('-w', '--window', type=int, default=3, help='Sliding window size')
 
     #command line args for server spesific functionality 
-    parser.add_argument('-d', '--discard', type=int, help='Sequence number to discard')
-    parser.add_argument()
+    parser.add_argument('-d', '--discard', type=int, help='Sequence number to discard (Test)')
+    
+    #common command line args 
+    parser.add_argument('-i', '--ip', type=str, default='127.0.0.1', help='IP address')
+    parser.add_argument('-p', '--port', type=int, default=8080, help='Port number')
 
     args = parser.parse_args()
 
@@ -43,47 +45,31 @@ def parse_args():
 def main():
     """
     Main method for this application 
-
-
     """  
     args = parse_args()
 
     try:
         if args.server:
-        #servermode
+        #server mode
             server = Server(args.ip, args.port, discarded_seq=args.discard)
             out_file="file.txt"
             server.recieve_file(out_file)
 
         elif args.client:
+        #client mode
+            client = Client(args.ip, args.port, args.window)
+            client.send_file(args.file)
+
             
+   except KeyboardInterrupt:
+        print("\nApplication terminated by user")
+        sys.exit(0)
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
         
          
             
-        
-        
-        
-
-
-        
-
-    
-
-    
-
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
